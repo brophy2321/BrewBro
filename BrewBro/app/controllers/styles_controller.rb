fc32e7fb95d50a06e1def76d001be0a3211259ab
RYANBROPHY <BROPHY2321@ME.COM>
2016-10-18 17:11:16 -0400

    add show

--- BrewBro/app/controllers/styles_controller.rb
+++ BrewBro/app/controllers/styles_controller.rb
@@ -1,9 +1,23 @@
 class StylesController < ApplicationController
+def index
+  @styles = Style.all
+end
+
+  def show
+    @style = Style.find(params[ :id])
+  end
+
   def new
   end
+
   def create
-    @style = Style.new(params[:article])
+    @style = Style.new(style_params)
     @style.save
     redirect_to @style
   end
+  
+  private
+    def style_params
+        params.require( :style).permit( :style)
+      end
 end
